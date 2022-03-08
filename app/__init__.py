66ac63a6ac5af900b02f5d9f16ece266af6ed70a
BuildTools <unconfigured@null.spigotmc.org>
2022-03-08 18:29:35 -0500

    Temporary flask app running

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,20 @@
-oooo_O
+# Doink!: Lia, LTW, Lisel, Tomas
+# P02
+# Period 1
+
+from flask import Flask, render_template, request, session, redirect
+import sqlite3
+
+app = Flask(__name__)    #create Flask object
+
+@app.route("/")
+def disp_homePage():
+    return render_template("login.html")
+
+@app.route("/register")
+def disp_registerPage():
+    return render_template("register.html")
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()

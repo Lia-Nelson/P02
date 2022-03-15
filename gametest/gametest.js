0c2b000b39473450f6ccb6abcba5ee06e13a2128
tomas-20 <tacuna20@stuy.edu>
2022-03-15 08:27:42 -0400

    replaced the drawMusicNote function with a quarterNote and halfNote function

--- gametest/gametest.js
+++ gametest/gametest.js
@@ -1,12 +1,22 @@
 let ctx = slate.getContext("2d");
 let TAU = Math.PI * 2;
+let TILT = - Math.PI / 8;
 
-function drawMusicNote(e) {
+function quarterNote(e) {
   console.log("printing note");
   ctx.beginPath();
-  ctx.ellipse(e.offsetX, e.offsetY, 10, 8, - Math.PI / 8, 0, TAU);
+  ctx.ellipse(e.offsetX, e.offsetY, 10, 8, TILT, 0, TAU);
   ctx.fill();
   ctx.fillRect(e.offsetX + 8, e.offsetY + 2, 1.5, -50);
 }
 
-slate.addEventListener("click", drawMusicNote);
+function halfNote(e) {
+  console.log("printing note");
+  ctx.beginPath();
+  ctx.ellipse(e.offsetX, e.offsetY, 10, 8, TILT, 0, TAU);
+  ctx.lineWidth = 2;
+  ctx.stroke();
+  ctx.fillRect(e.offsetX + 8, e.offsetY + 2, 1.5, -50);
+}
+
+slate.addEventListener("click", halfNote);

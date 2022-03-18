90db7757cac48c276297ea7637d106073de67418
tomas-20 <tacuna20@stuy.edu>
2022-03-18 08:43:36 -0400

    added boxWidth, boxColor, sliderWidth, and sliderColor

--- gametest/canvas.js
+++ gametest/canvas.js
@@ -5,13 +5,19 @@ let height = 70;
 let space = 20;
 let rows = 2;
 let cols = 4;
-let sh = 75;
+let sliderHeight = 75;
+let boxWidth = 1;
+let boxColor = "#000000";
+let siderWidth = 2;
+let sliderColor = "#00d990";
 
 let colWidth = width / cols;
 let x = (slate.width - width) / 2;
 let fullHeight = height + space;
 let startY = (slate.height + space - rows * fullHeight) / 2;
-let ssy = startY + (height - sh) / 2;
+let ssy = startY + (height - sliderHeight) / 2;
+
+ctx.lineWidth = lineWidth;
 
 function drawRows() {
   for (let row = 0; row < rows; row ++) {
@@ -26,5 +32,7 @@ function drawSlider(time) {
   let pos = time * rows;
   let rowPos = pos % 1;
   let row = pos - rowPos;
-  ctx.strokeRect(x + width * rowPos, ssy + fullHeight * row, 0, sh);
+  console.log(x + width * rowPos);
+  console.log(ssy + fullHeight * row);
+  ctx.fillRect(x + width * rowPos, ssy + fullHeight * row, sliderWidth, sliderHeight);
 }

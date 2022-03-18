ef4c6f5d0ecc2b8112d348e3a38d883965676602
Lia-Nelson <jnelson20@stuy.edu>
2022-03-18 08:41:15 -0400

    rhythm generation functions working

--- noises/notes.py
+++ noises/notes.py
@@ -1,66 +1,82 @@
 import random
 
-class Notes:
-  # # note gets beat
-  # beatNote = 4
-  #
-  # # beats per measure
-  # bpm = 4
+# # note gets beat
+# beatNote = 4
+#
+# # beats per measure
+# bpm = 4
 
-  # number of measures
-  measures = 8
+# number of measures
+measures = 8
 
-  notes = [
-    {"duration": 1/16, "triplet": True},
-    {"duration": 1/8, "triplet": True},
-    {"duration": 3/16, "triplet": False},
-    {"duration": 1/4, "triplet": True},
-    {"duration": 3/8, "triplet": False},
-    {"duration": 1/2, "triplet": True},
-    {"duration": 3/4, "triplet": False},
-    {"duration": 1, "triplet": True},
-    {"duration": 3/2, "triplet": False}
-  ]
+notes = [
+  {"duration": 1/16, "triplet": False},
+  {"duration": 1/8, "triplet": True},
+  {"duration": 3/16, "triplet": False},
+  {"duration": 1/4, "triplet": True},
+  {"duration": 3/8, "triplet": False},
+  {"duration": 1/2, "triplet": True},
+  {"duration": 3/4, "triplet": False},
+  {"duration": 1, "triplet": True},
+  {"duration": 3/2, "triplet": False}
+]
 
-  def generate_rhythms(bpm:int, beatNote:int) -> list:
-    generated = []
-
-    for i in range(measures):
-      # print(i)
-      remaining = bpm / beatNote
-      maxIndex = len(notes) - 1
-      # print(remaining)
-      # print(maxIndex)
-      while remaining > 0:
-          index = random.randint(0, maxIndex)
-          selection = notes[index]
-          # print(selection)
-          duration = selection["duration"]
-          # print(duration)
-          if (duration > remaining):
-            maxIndex = index - 1;
+def generate_rhythms(bpm:int, beatNote:int) -> list:
+  generated = []
+  print("Measures: " + str(measures))
+  print("Beats per measure: " + str(bpm))
+  print("Beat note: " + str(beatNote))
+  for i in range(measures):
+    in_measure = []
+    print("Current measure: " + str(i))
+    remaining = bpm / beatNote
+    maxIndex = len(notes) - 1
+    print(remaining)
+    print(maxIndex)
+    while remaining > 0:
+      index = random.randint(0, maxIndex)
+      selection = notes[index]
+      # print(selection)
+      duration = selection["duration"]
+      # print(duration)
+      if (duration > remaining):
+        maxIndex = index - 1;
+      else:
+        note = {"duration": duration}
+        remaining -= duration
+        # in_measure.append(duration)
+        # code = str(duration) + "%@!"
+        # one in three chance of being a rest
+        if (random.randint(0,2) == 0):
+          note["type"] = "rest"
+          note["triplet"] = False
+          # in_measure.append("rest")
+          # in_measure.append(False)
+          # code += "R%@!F"
+        else:
+          note["type"] = "note"
+          # in_measure.append("note")
+          if (
+          (selection["triplet"] == True) and
+          # from 2/3 chance of notes, 1/3 chance
+          # of being made a triplet
+          (random.randint(0, 2) == 0)
+          ):
+           note["triplet"] = True
+           # in_measure.append(True)
+           # code += "N%@!T"
           else:
-            remaining -= duration
-            code = str(duration) + "%@!"
-            # one in three chance of being a rest
-            if (random.randint(0,2) == 0):
-              code += "R%@!F"
-            else:
-              if (
-              (selection["triplet"] == True) and
-              # from 2/3 chance of notes, 1/3 chance
-              # of being made a triplet
-              (random.randint(0, 2) == 0)
-              ):
-                code += "N%@!T"
-              else:
-                code += "N%@!F"
+            note["triplet"] = False
+            # in_measure.append(False)
+            # code += "N%@!F"
             print("measure: " + str(i))
             print("remaining: " + str(remaining))
-            print("code: " + code)
-            generated.append(code)
-            return generated
+            # print("code: " + code)
+            # generated.append(code)
+      in_measure.append(note)
+    generated.append(in_measure)
+  return generated
 
-print(Notes.generate_rhythms(4, 4))
-print(Notes.generate_rhythms(3, 8))
-    # print(generated)
+print(generate_rhythms(4, 4))
+print(generate_rhythms(3, 8))
+# print(generated)

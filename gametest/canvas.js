fe8a28453678abf4e3933bab17bae83690a028b2
tomas-20 <tacuna20@stuy.edu>
2022-03-17 22:48:24 -0400

    replaced the width argument of drawRow with colWidth

--- gametest/canvas.js
+++ gametest/canvas.js
@@ -1,18 +1,18 @@
 let slate = document.getElementById("slate");
 let ctx = slate.getContext("2d");
 
-function drawRow(x, y, width, height, cols) {
-  let colWidth = width / cols;
+function drawRow(x, y, colWidth, height, cols) {
   for (let i = 0; i < cols; i ++) {
     ctx.strokeRect(x + colWidth * i, y, colWidth, height);
   }
 }
 
 function drawRows(width, height, space, rows, cols) {
+  let colWidth = width / cols;
   let x = (slate.width - width) / 2;
   let fullHeight = height + space;
   let startY = (slate.height + space - rows * fullHeight) / 2;
   for (let i = 0; i < rows; i ++) {
-    drawRow(x, startY + fullHeight * i, width, height, cols);
+    drawRow(x, startY + fullHeight * i, colWidth, height, cols);
   }
 }

443984f751daac49817851bd6deb85fecdd3421e
tomas-20 <tacuna20@stuy.edu>
2022-03-17 23:02:58 -0400

    got rid of the drawRow helper function

--- gametest/canvas.js
+++ gametest/canvas.js
@@ -6,18 +6,15 @@ let space = 20;
 let rows = 2;
 let cols = 4;
 
-function drawRow(x, y, colWidth) {
-  for (let col = 0; col < cols; col ++) {
-    ctx.strokeRect(x + colWidth * col, y, colWidth, height);
-  }
-}
-
 function drawRows() {
   let colWidth = width / cols;
   let x = (slate.width - width) / 2;
   let fullHeight = height + space;
   let startY = (slate.height + space - rows * fullHeight) / 2;
   for (let row = 0; row < rows; row ++) {
-    drawRow(x, startY + fullHeight * row, colWidth);
+    for (let col = 0; col < cols; col ++) {
+      let y = startY + fullHeight * row;
+      ctx.strokeRect(x + colWidth * col, y, colWidth, height);
+    }
   }
 }

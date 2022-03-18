381f9610ad85b4917d006833f9c04d92e6807eba
tomas-20 <tacuna20@stuy.edu>
2022-03-17 22:57:27 -0400

    made width, height, space, rows, and cols into global variables

--- gametest/canvas.js
+++ gametest/canvas.js
@@ -1,18 +1,23 @@
 let slate = document.getElementById("slate");
 let ctx = slate.getContext("2d");
+let width = 400;
+let height = 70;
+let space = 20;
+let rows = 2;
+let cols = 4;
 
-function drawRow(x, y, colWidth, height, cols) {
+function drawRow(x, y, colWidth) {
   for (let i = 0; i < cols; i ++) {
     ctx.strokeRect(x + colWidth * i, y, colWidth, height);
   }
 }
 
-function drawRows(width, height, space, rows, cols) {
+function drawRows() {
   let colWidth = width / cols;
   let x = (slate.width - width) / 2;
   let fullHeight = height + space;
   let startY = (slate.height + space - rows * fullHeight) / 2;
   for (let i = 0; i < rows; i ++) {
-    drawRow(x, startY + fullHeight * i, colWidth, height, cols);
+    drawRow(x, startY + fullHeight * i, colWidth);
   }
 }

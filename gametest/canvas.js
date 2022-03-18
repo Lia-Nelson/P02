7249366594ce1709812dd8eb61ff194a8953dfb7
tomas-20 <tacuna20@stuy.edu>
2022-03-17 23:29:13 -0400

    added the drawSlider function

--- gametest/canvas.js
+++ gametest/canvas.js
@@ -5,16 +5,26 @@ let height = 70;
 let space = 20;
 let rows = 2;
 let cols = 4;
+let sh = 75;
+
+let colWidth = width / cols;
+let x = (slate.width - width) / 2;
+let fullHeight = height + space;
+let startY = (slate.height + space - rows * fullHeight) / 2;
+let so = (height - sh) / 2;
 
 function drawRows() {
-  let colWidth = width / cols;
-  let x = (slate.width - width) / 2;
-  let fullHeight = height + space;
-  let startY = (slate.height + space - rows * fullHeight) / 2;
   for (let row = 0; row < rows; row ++) {
     for (let col = 0; col < cols; col ++) {
       let y = startY + fullHeight * row;
       ctx.strokeRect(x + colWidth * col, y, colWidth, height);
     }
   }
+}
+
+function drawSlider(time) {
+  let pos = time * rows;
+  let rowPos = pos % 1;
+  let row = pos - rowPos;
+  ctx.strokeRect(rowPos * width, startY + fullHeight * row + so, 0, sh);
 }

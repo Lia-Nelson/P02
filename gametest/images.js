c0a723e80d0e836d2b9c39cb88415bef996f492c
tomas-20 <tacuna20@stuy.edu>
2022-03-23 19:34:48 -0400

    added the half note image I forgot before

--- gametest/images.js
+++ gametest/images.js
@@ -2,3 +2,5 @@ let quarterNote = new Image();
 quarterNote.src = "images/quarter_note.svg";
 let halfNote = new Image();
 halfNote.src = "images/half_note.svg";
+let wholeNote = new Image();
+wholeNote.src = "images/whole_note.svg";

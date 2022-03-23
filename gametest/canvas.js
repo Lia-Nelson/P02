3d8968ee07b31907a0f2defb747f5bc4c62d7fe7
tomas-20 <tacuna20@stuy.edu>
2022-03-23 18:38:18 -0400

    renamed width and height to boxWidth and boxHeight

--- gametest/canvas.js
+++ gametest/canvas.js
@@ -1,25 +1,23 @@
 let slate = document.getElementById("slate");
 let ctx = slate.getContext("2d");
-let width = 400;
-let height = 70;
+let boxWidth = 400;
+let boxHeight = 70;
 let space = 20;
 let rows = 2;
 let cols = 4;
 let sliderHeight = 75;
-let noteHeight = 60;
-let boxWidth = 1;
+let boxThick = 1;
 let boxColor = "#000000";
 let sliderWidth = 2;
 let sliderColor = "#00d990";
 let totalTime = 5000;
 
-let colWidth = width / cols;
-let startX = (slate.width - width) / 2;
-let fullHeight = height + space;
+let colWidth = boxWidth / cols;
+let startX = (slate.width - boxWidth) / 2;
+let fullHeight = boxHeight + space;
 let startY = (slate.height + space - rows * fullHeight) / 2;
-let ssy = (height - sliderHeight) / 2;
 
-ctx.lineWidth = boxWidth;
+ctx.lineWidth = boxThick;
 ctx.strokeStyle = boxColor;
 ctx.fillStyle = sliderColor;
 
@@ -27,8 +25,8 @@ function drawThing(func, ptime, thingWidth, thingHeight) {
   let pos = ptime * rows;
   let rowPos = pos % 1;
   let row = pos - rowPos;
-  let x = startX + width * rowPos - thingWidth / 2;
-  let y = startY + fullHeight * row + (height - thingHeight) / 2;
+  let x = startX + boxWidth * rowPos - thingWidth / 2;
+  let y = startY + fullHeight * row + (boxHeight - thingHeight) / 2;
   func(x, y, thingWidth, thingHeight);
 }
 
@@ -36,7 +34,7 @@ function drawRows() {
   for (let row = 0; row < rows; row ++) {
     for (let col = 0; col < cols; col ++) {
       let y = startY + fullHeight * row;
-      ctx.strokeRect(startX + colWidth * col, y, colWidth, height);
+      ctx.strokeRect(startX + colWidth * col, y, colWidth, boxHeight);
     }
   }
 }

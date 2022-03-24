dc8d33027f70b2fdd0223abb540c771d302c8ace
tomas-20 <tacuna20@stuy.edu>
2022-03-24 01:43:13 -0400

    once again changed some if statements to check if not logged in rather than if logged in

--- app/__init__.py
+++ app/__init__.py
@@ -110,15 +110,15 @@ def auth():
 @app.route("/home")
 def disp_home():
     #check login_method()
-    if logged_in(): #later to be replaced with check login
-        user = session["user"]
-        highscore = database.display_score(user)
-        global score
-        score = 0
-        global lives
-        lives = 3
-        return render_template("home.html", username = user, score = highscore)
-    return render_template("wrong.html") #if not logged in, give error
+    if not logged_in():
+        return render_template("wrong.html") #if not logged in, give error
+    user = session["user"]
+    highscore = database.display_score(user)
+    global score
+    score = 0
+    global lives
+    lives = 3
+    return render_template("home.html", username = user, score = highscore)
 
 @app.route("/instruct")
 def disp_Instructions():
@@ -153,34 +153,34 @@ def changeBot(page_id):
 
 @app.route("/game")
 def disp_gamePage():
-    if logged_in():
-        global tempo_Top
-        global tempo_Bot
-        global score
-        global lives
-        return render_template("gamescreen.html", top = tempo_Top, bot = tempo_Bot, score = score, lives = lives)
-    return render_template("wrong.html")
+    if not logged_in():
+        return render_template("wrong.html")
+    global tempo_Top
+    global tempo_Bot
+    global score
+    global lives
+    return render_template("gamescreen.html", top = tempo_Top, bot = tempo_Bot, score = score, lives = lives)
 
 @app.route("/endgame/<nscore>/<nlives>")
 def record_results(nscore, nlives):
-    if logged_in():
-        global score
-        score = nscore
-        global lives
-        lives = nlives
-        return redirect("/results")
-    return render_template("wrong.html")
+    if not logged_in():
+        return render_template("wrong.html")
+    global score
+    score = nscore
+    global lives
+    lives = nlives
+    return redirect("/results")
 
 @app.route("/results")
 def disp_results():
-    if logged_in():
-        global score
-        old_score = database.display_score(session["user"])
-        yay = int(score) > int(old_score)
-        if yay:
-            database.update_score(session["user"], score)
-        return  render_template("results.html", score = score, newhighscore = yay)
-    return render_template("wrong.html")
+    if not logged_in():
+        return render_template("wrong.html")
+    global score
+    old_score = database.display_score(session["user"])
+    yay = int(score) > int(old_score)
+    if yay:
+        database.update_score(session["user"], score)
+    return  render_template("results.html", score = score, newhighscore = yay)
 
 if __name__ == "__main__":
     app.debug = True
